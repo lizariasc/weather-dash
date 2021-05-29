a7ca9439399f4b98151df5945c8efe3c4d5f05d4
LAgrl <lizethariasc14@gmail.com>
2021-05-28 19:30:58 -0700

    5-day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,7 +48,7 @@ function currentWeather(city){
         // The date format method is taken from the  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
         var date=new Date(response.dt*1000).toLocaleDateString();
 
-        //parse the response for name of city and concanatig the date and icon.
+        // Date 
         $(currentCity).html(response.name +" ("+date+")" + "<img src="+iconurl+">");
 
         // parse the response to display the current temperature and convert the temp to fahrenheit
@@ -56,12 +56,12 @@ function currentWeather(city){
         $(currentTemperature).html((tempF).toFixed(2)+"&#8457");
 
         // Humidity
-        $(currentHumidty).html(response.main.humidity+"%");
+        $(currentHumidty).html(response.main.humidity+" %");
 
         // Wind
-        var ws=response.wind.speed;
-        var windsmph=(ws*2.237).toFixed(1);
-        $(currentWSpeed).html(windsmph+"MPH");
+        var ws = response.wind.speed;
+        var windsmph = (ws*2.237).toFixed(2);
+        $(currentWSpeed).html(windsmph+" MPH");
 
 
         // Display UVIndex
@@ -100,6 +100,39 @@ function UVIndex(ln,lt){
             });
 }
 
+// 5-day forecast 
+function forecast(cityid){
+    var dayover= false;
+    var queryforcastURL="https://api.openweathermap.org/data/2.5/forecast?id="+cityid+"&appid="+APIKey;
+    $.ajax({
+        url:queryforcastURL,
+        method:"GET"
+    }).then(function(response){
+        
+        for (i=0;i<5;i++){
+            var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+            var iconcode= response.list[((i+1)*8)-1].weather[0].icon;
+            var iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
+            var tempK= response.list[((i+1)*8)-1].main.temp;
+            var tempF =(((tempK-273.5)*1.80)+32).toFixed(2);
+            var windsmph = response.list[((i+1)*8)-1].wind.speed;
+            var humidity= response.list[((i+1)*8)-1].main.humidity;
+        
+            $("#fDate"+i).html(date);
+            $("#fImg"+i).html("<img src="+iconurl+">");
+            $("#fTemp"+i).html(tempF+"&#8457");
+            $("#fWind"+i).html(windsmph+"MPH");
+            $("#fHumidity"+i).html(humidity+"%");
+         
+        }
+        
+    });
+}
 
-// Event listeners 
-$("#search-button").on("click", displayWeather);
+
+// Event listeners
+$("#search-button").on("click",displayWeather);
+
+
+
+
