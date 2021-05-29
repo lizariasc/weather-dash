62b3d022cd9372e4a03bfee8d45d42080098b0c3
LAgrl <lizethariasc14@gmail.com>
2021-05-28 18:15:31 -0700

    display city weather and date

---
+++ assets/js/script.js
@@ -0,0 +1,72 @@
+// Variables
+var city="";
+
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
+
+function find(c){
+    for (var i=0; i<sCity.length; i++){
+        if(c.toUpperCase()===sCity[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
+
+// API key
+var APIKey="a0aca8a89948154a4182dcecc780b513";
+
+// Display weather 
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
+
+function currentWeather(city){
+    // fetch
+    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    $.ajax({
+        url:queryURL,
+        method:"GET",
+    }).then(function(response){
+
+        // parse the response to display the current weather including the City name
+        console.log(response);
+        
+        var weathericon= response.weather[0].icon;
+        var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
+        // The date format method is taken from the  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+        var date=new Date(response.dt*1000).toLocaleDateString();
+
+        //parse the response for name of city and concanatig the date and icon.
+        $(currentCity).html(response.name +" ("+date+")" + "<img src="+iconurl+">");
+
+        // parse the response to display the current temperature and convert the temp to fahrenheit
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        $(currentTemperature).html((tempF).toFixed(2)+"&#8457");
+
+        // Humidity
+        $(currentHumidty).html(response.main.humidity+"%");
+
+        // Wind
+        var ws=response.wind.speed;
+        var windsmph=(ws*2.237).toFixed(1);
+        $(currentWSpeed).html(windsmph+"MPH");
+
+    })
+};
+
+
+
+//Click Handlers
+$("#search-button").on("click",displayWeather);
